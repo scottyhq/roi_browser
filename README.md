abdf5918d563467109e5f18b51ffa4db916cbc3b
Scott Henderson <scottyhq@gmail.com>
2015-08-22 17:21:27 -0500

    added environment.yml, changed roi_browser.py to use python3

--- README.md
+++ README.md
@@ -5,21 +5,36 @@ Python browser to flip through directory of interferograms (.int or .unw)
 
 *Be warned*, it can be a bit slow for full resolution images, but it's super useful! 
 
-You need a functional python installation to run the browser. I use [Enthought's Canopy Software](https://www.enthought.com/products/canopy/), but as long as you have the following, it should work:
+You need a functional Python installation to run the browser. I recommend using the free [Continuum Anaconda Distribution](https://store.continuum.io/cshop/anaconda/). Using the *conda* command line tool, you can easily ensure the proper dependencies are installed (see instructions below).
 
-* python 2.7.3
-* matplotlib 1.3.0
-* numpy 1.7.1
-
-Usage
------
+Installation
+------------
 To run the browser on your machine do the following:
 
-	cd install-here
 	git clone https://github.com/scottyhq/roi_browser
-	echo PATH=$PATH:install-here/roi_browser
-	roi_browser.py 'int*/filt*unw'
+	cd roi_browser
+	conda env create
+	
+To link an executable run the following (adjusting directories as needed):
+	
+	ln -s /path-to-roi_browser/roi_browser.py /user/local/bin/roi_browser 
+	
+
+Usage
+_----
+	
+	source activate roi_browser
+	roi_browse --help
+	roi_browser.py 'roi_browser_pathexample/int*/filt*unw'
 
 You should then see a window like this:
 ![Screenshot](/screen_shot.png)
 
+
+Troubleshooting
+---------------
+See [this useful introduction and guide](http://continuum.io/blog/conda-data-science) to conda Python environments. To return to your default environment run:
+	
+	source deactivate
+
+By default the browser uses the 'TkAgg' matplotlib backend since it is the default. You can change to another by modifying line #10 in the roi_browser.py source code (e.g. matplotlib.use('Qt'))
--- roi_browser.py
+++ roi_browser.py
@@ -5,29 +5,24 @@ Browse though interferograms in a directory
 Usage: roi_browser.py "./int*/filt*unw"
 Note: designed for 'int' and 'unw' files... but wouldn't take much effort to handle others...
 Author: Scott Henderson (st54@cornell.edu)
-Requires:
-    * python 2.7
-    * matplotlib 1.3
-    * numpy 1.7
-
-Planned Improvements:
-	- option to print basic statistics to terminal
-	- option to interactively change colorbar
 """
-
-import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, MultiCursor
 from mpl_toolkits.axes_grid1 import ImageGrid, make_axes_locatable
+from matplotlib.colors import LogNorm # for radar amplitude data
+
 import argparse
 import os
 import re
 import glob
 import sys
-from matplotlib.colors import LogNorm # for radar amplitude data
+import numpy as np
 
+matplotlib.use('TkAgg')
 __author__ = 'Scott Henderson'
-__version__ = '1.0'
+__version__ = '0.1'
 
 def format_coord(x, y):
 	"""
@@ -62,34 +57,34 @@ def format_coord2(x,y):
 
 
 def get_files(filetype):
-    """
+	"""
 	Make a dictionary of dates:filenames
 	"""
-    igramsDict = {}
-    path = os.path.expanduser(filetype) # make sure ~ in path is expanded
-    paths = glob.glob(path)
-    for path in paths:
+	igramsDict = {}
+	path = os.path.expanduser(filetype) # make sure ~ in path is expanded
+	paths = glob.glob(path)
+	for path in paths:
 		filename = os.path.basename(path)
 		datePair = re.search("\d{6}-\d{6}",filename).group()
 		igramsDict[datePair] = path
-    return igramsDict
+	return igramsDict
 
 
 def load_rsc(path):
-    """
-    Read metadata from .rsc file into python dictionary
-    """
-    metadata = {}
-    rsc = open(path + '.rsc', 'r')
-    # Remove blank lines, retaining only lines with key, value pairs
-    allLines = [line for line in rsc.readlines() if line.strip()]
-    for line in allLines:
+	"""
+	Read metadata from .rsc file into python dictionary
+	"""
+	metadata = {}
+	rsc = open(path + '.rsc', 'r')
+	# Remove blank lines, retaining only lines with key, value pairs
+	allLines = [line for line in rsc.readlines() if line.strip()]
+	for line in allLines:
 		items = line.split()
 		var = items[0]
 		value = '_'.join(items[1:]) #replace space w/underscore
 		metadata[var] = value
-    rsc.close()
-    return metadata
+	rsc.close()
+	return metadata
 
 
 def load_data(igramPath, args):
@@ -98,7 +93,7 @@ def load_data(igramPath, args):
 	"""
 	metadata = load_rsc(igramPath)
 	if args.verbose:
-		print 'Path:\t\t{0}\nWavelength:\t{WAVELENGTH}\nOrbit:\t\t{ORBIT_DIRECTION}\nTimespan:\t{TIME_SPAN_YEAR}\nLength:\t\t{FILE_LENGTH}\nWidth:\t\t{WIDTH}\n'.format(igramPath,**metadata)
+		print('Path:\t\t{0}\nWavelength:\t{WAVELENGTH}\nOrbit:\t\t{ORBIT_DIRECTION}\nTimespan:\t{TIME_SPAN_YEAR}\nLength:\t\t{FILE_LENGTH}\nWidth:\t\t{WIDTH}\n'.format(igramPath,**metadata))
 
 	dims = (int(metadata['FILE_LENGTH']), int(metadata['WIDTH']))
 	if igramPath.endswith('int'):
@@ -136,7 +131,7 @@ def load_unw(igramPath, dims):
 	amp = phs.copy()
 
 	with open(igramPath,'rb') as f:
-		rows = range(dims[0]-1)
+		rows = list(range(dims[0]-1))
 		# Set offset to read phase array
 		junk = np.fromfile(f, dtype=datatype, count=dims[1]) 
 		for i in rows:
@@ -168,13 +163,13 @@ def create_single_browser(igramsDict, args):
 	"""
 	Just show phase data
 	"""
-	order = igramsDict.keys()
+	order = list(igramsDict.keys())
 	order.sort()
 	dates = order[0]
 	igPath = igramsDict[dates]
 
 	junk,data = load_data(igPath, args)
-    
+	
 	fig = plt.figure(figsize=(8.5,11))
 	titlestr = os.path.basename(igPath)
 	title = fig.suptitle(titlestr, fontsize=14, fontweight='bold')
@@ -185,7 +180,7 @@ def create_single_browser(igramsDict, args):
 	
 	divider = make_axes_locatable(ax)
 	cax = divider.append_axes('right',size='5%',pad=0.05)
-	cb = plt.colorbar(im, cax=cax)    
+	cb = plt.colorbar(im, cax=cax)	  
 	if args.displacement:
 		cb.set_label('dlos [m]')
 	else:
@@ -199,11 +194,11 @@ def create_single_browser(igramsDict, args):
 	axcolor = 'lightgoldenrodyellow'
 	axIG = plt.axes([0.25, 0.02, 0.65, 0.03], axisbg=axcolor) 
 	sIG = Slider(axIG, 'IG #/{0}'.format(len(order)), 1, len(igramsDict),
-	               valinit=1,
-	               valfmt='%i',
-	               closedmin=True,
-	               closedmax=True,
-	               dragging=True)
+				   valinit=1,
+				   valfmt='%i',
+				   closedmin=True,
+				   closedmax=True,
+				   dragging=True)
 	
 	def update(val):
 		dates = order[int(sIG.val)-1]
@@ -216,17 +211,17 @@ def create_single_browser(igramsDict, args):
 		im.set_extent([-0.5, data.shape[1]-0.5, data.shape[0]-0.5, -0.5])
 		title.set_text(os.path.basename(igPath))
 		plt.draw()
-        
+		
 	def onpress(event):
 		if event.key not in ('n', 'p', 'left', 'right'): return
 		if event.key=='n' or event.key=='right': 
-		    newval = sIG.val + 1
+			newval = sIG.val + 1
 		elif event.key=='p' or event.key=='left':
-		    newval = sIG.val - 1
+			newval = sIG.val - 1
 		if newval < sIG.valmin: 
-		    newval = sIG.valmax
+			newval = sIG.valmax
 		if newval > sIG.valmax: 
-		    newval = sIG.valmin
+			newval = sIG.valmin
 		sIG.set_val(newval) 
 	
 	fig.canvas.mpl_connect('key_press_event', onpress)
@@ -239,7 +234,7 @@ def create_double_browser(igramsDict, args):
 	Show amplitude and phase side-by side 
 	"""
 	#print igramsDict
-	order = igramsDict.keys()
+	order = list(igramsDict.keys())
 	order.sort()
 	dates = order[0]
 	igPath = igramsDict[dates]
@@ -250,17 +245,17 @@ def create_double_browser(igramsDict, args):
 	title = fig.suptitle(titlestr, fontsize=14, fontweight='bold') 
 	
 	grid = ImageGrid(fig, 111, 
-                nrows_ncols = (1, 2),
-                direction="row",
-                axes_pad = 1.0,
-                add_all=True,
-                label_mode = 'all', 
-                share_all = True,
-                cbar_location='right', 
-                cbar_mode='each', 
-                cbar_size=0.1,
-                cbar_pad=0.0
-                )
+				nrows_ncols = (1, 2),
+				direction="row",
+				axes_pad = 1.0,
+				add_all=True,
+				label_mode = 'all', 
+				share_all = True,
+				cbar_location='right', 
+				cbar_mode='each', 
+				cbar_size=0.1,
+				cbar_pad=0.0
+				)
 	ax1 = grid[0]
 	ax2 = grid[1]
 	ax1.set_axis_bgcolor('gray')
@@ -276,9 +271,9 @@ def create_double_browser(igramsDict, args):
 		fmt = '%0.2f'
 
 	im1 = ax1.imshow(amp, origin='lower',
-                            cmap=plt.cm.gray,
-                            extent=[-0.5,amp.shape[1]-0.5,amp.shape[0]-0.5,-0.5],
-                            norm=norm)                         
+							cmap=plt.cm.gray,
+							extent=[-0.5,amp.shape[1]-0.5,amp.shape[0]-0.5,-0.5],
+							norm=norm)						   
 	ax1.cax.colorbar(im1, format=fmt)
 	
 	im2 = ax2.imshow(phs, origin='lower',cmap=plt.get_cmap(args.cmap))
@@ -302,11 +297,11 @@ def create_double_browser(igramsDict, args):
 	axcolor = 'lightgoldenrodyellow'
 	axIG = plt.axes([0.20, 0.05, 0.60, 0.02], axisbg=axcolor) 
 	sIG = Slider(axIG, 'IG #/{0}'.format(len(order)), 1, len(igramsDict),
-	               valinit=1,
-	               valfmt='%i',
-	               closedmin=True,
-	               closedmax=True,
-	               dragging=True)
+				   valinit=1,
+				   valfmt='%i',
+				   closedmin=True,
+				   closedmax=True,
+				   dragging=True)
 	
 	def update(val):
 		dates = order[int(sIG.val)-1]
@@ -329,18 +324,18 @@ def create_double_browser(igramsDict, args):
 		
 		title.set_text( os.path.basename(igPath) )
 		plt.draw()
-        
+		
 	def onpress(event):
 		if event.key not in ('n', 'p', 'right', 'left'): return
 		if event.key=='n' or event.key=='right': 
-		    newval = sIG.val + 1
+			newval = sIG.val + 1
 		elif event.key=='p' or event.key=='left':  
-		    newval = sIG.val - 1
+			newval = sIG.val - 1
 		
 		if newval < sIG.valmin: 
-		    newval = sIG.valmax
+			newval = sIG.valmax
 		if newval > sIG.valmax: 
-		    newval = sIG.valmin
+			newval = sIG.valmin
 		
 		sIG.set_val(newval) #update() automatically called
 	
@@ -363,23 +358,22 @@ def main():
 	parser.add_argument('-x','--cursor',action='store_true',default=False,help='show cursorin both amp and phs windows (need to also use -a)')
 	parser.add_argument('-a','--amp', action='store_true', default=False, help='show radar amplitude alongside phase')
 	parser.add_argument('-d','--displacement', action='store_true', default=False, help='convert unwrapped phase to displacement [m]')
-	#parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 	parser.add_argument("-v", "--verbose", default=False, help="print interferogram to terminal", action="store_true")
 	parser.add_argument('--version', action='version', version='{0}'.format(__version__))
 	
 	args = parser.parse_args()
 
 
-	print '\n=================== roi_browser {} ===========================\n'.format(__version__)
-	print '\n*Use right and left arrow keys to change cycle images. Or click on the slider.\n '
+	print('\n=================== roi_browser {} ===========================\n'.format(__version__))
+	print('\n*Use right and left arrow keys to change cycle images. Or click on the slider.\n ')
 
 	if args.displacement==True and not args.files.endswith('unw'):
-		print 'Warning: only .unw (unwrapped) files can be converted to displacement... [m]\n'
+		print('Warning: only .unw (unwrapped) files can be converted to displacement... [m]\n')
 		args.displacement=False
 	
 	igrams = get_files(args.files)
 	if len(igrams)==0:
-		print "Error: No files matching search: {}\n".format(args.files)
+		print("Error: No files matching search: {}\n".format(args.files))
 		sys.exit()
 	
 	if args.amp:
@@ -389,5 +383,4 @@ def main():
 
 
 if __name__ == '__main__':
-    #print 'new version'
 	main()
